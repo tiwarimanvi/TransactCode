13053731fa8166be8bc09d08b7b94a83bc966eab
Shreyas <shreyassingh621@gmail.com>
2023-06-21 03:46:52 +0530

    Update 1 on Navbar

--- transac_code/src/components/Navigation.jsx
+++ transac_code/src/components/Navigation.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Navigation (){
     return(
-        <div className='flex justify-center bg-[#013914]'>
+        <div className='flex flex-col items-center bg-[#013914]'>
             <div className='w-[1400px] flex justify-between text-white my-[20px]'>
                 <div className='text-[30px] font-montserrat font-bold'>
                     <span className=''>transactcode</span>
@@ -15,6 +15,12 @@ function Navigation (){
                     <button className='px-[20px] py-[10px] text-white bg-transparent border rounded border-solid border-white hover:bg-[#1dbf73] hover:border-[#1dbf73] '>Sell</button>
                 </div>
             </div>
+            <hr className='w-full h-0 border-solid border-slate-600'></hr>
+            <div className='flex justify-between font-light w-[1400px] py-[10px] text-gray-500'>
+                <span>Test</span>
+                <span>Test2</span>                
+            </div>
+            
         </div>
     )
 }
