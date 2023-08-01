663bce15d352bc44011c9dad9bef92a7d0a39e85
Shreyas <shreyassingh621@gmail.com>
2023-08-01 09:05:54 +0530

    Revert "Pushing before swtiching to lottery taquito"
    This reverts commit 818b79063a974102cfa5091fb4bf081119deb146.

--- transac_code/src/App.jsx
+++ transac_code/src/App.jsx
@@ -1,11 +1,9 @@
 import Navbar from "./components/Navbar";
-import FileShare from "./components/FileShare";
 
 const App = () => {
   return (
     <div className="">
       <Navbar />
-      <FileShare/>
     </div>
   );
 };
