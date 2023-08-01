818b79063a974102cfa5091fb4bf081119deb146
Shreyas <shreyassingh621@gmail.com>
2023-08-01 08:35:23 +0530

    Pushing before swtiching to lottery taquito

--- transac_code/src/App.jsx
+++ transac_code/src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar";
+import FileShare from "./components/FileShare";
 
 const App = () => {
   return (
     <div className="">
       <Navbar />
+      <FileShare/>
     </div>
   );
 };
---
+++ transac_code/src/components/FileShare.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function FileShare(){
+    return(
+        <div>
+
+        </div>
+    )
+}
+export default FileShare;
